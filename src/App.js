6ac82390e3ddfae0338aeeb0d852615175d2a223
PaulinaOrellana <paplina@gmail.com>
2022-12-24 17:41:06 -0300

    Desafio 2 React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Input from "./Componentes/Input.jsx";
+import Boton from "./Componentes/Boton.jsx";
+ 
 function App() {
+    const [nombre, setNombre] = useState('');
+    const [password, setPassword] = useState('');
+ 
+  const BotonFormulario = () => {
+        if (password === '252525') {
+        return (<Boton></Boton>)
+    } else {
+      return (<></>)
+     }
+    }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">  
+      <h1>Desafío de Estados de Componentes y Eventos</h1>
+     
+      <Input
+      nombre={nombre}
+      setNombre={setNombre}
+      password={password}
+      setPassword={setPassword}
+      >
+      </Input>
+    <BotonFormulario></BotonFormulario>
+      </div>
   );
 }
-
+ 
 export default App;
---
+++ src/Componentes/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+ 
+const Boton = () => {
+  return (
+        <Button variant="success" type="button">
+           Contraseña Correcta 
+        </Button>
+  )
+}
+ 
+export default Boton
